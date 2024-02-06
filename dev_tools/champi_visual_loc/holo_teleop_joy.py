ea84dc9aa2e1199cfee2bead7ba50c559c430b44
André Russo <andrerusso02@gmail.com>
2024-02-06 14:33:09 +0100

    add gamepad teleop

---
+++ dev_tools/champi_visual_loc/holo_teleop_joy.py
@@ -0,0 +1,67 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Joy
+from geometry_msgs.msg import Twist
+
+
+class HoloTeleopJoy(Node):
+
+    def __init__(self):
+        super().__init__('visual_loc')
+
+        self.subscription = self.create_subscription(
+            Joy,
+            '/joy',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+        self.latest_msg = None
+
+        timer_period = 0.05  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+        self.pub = self.create_publisher(Twist, '/cmd_vel', 10)
+
+
+        self.max_linear_speed = 0.5 # m/s
+        self.max_angular_speed = 1.0 # rad/s
+
+
+
+
+    def listener_callback(self, msg):
+        self.latest_msg = msg
+
+
+    def timer_callback(self):
+        if self.latest_msg is not None:
+            self.pub.publish(self.joy_to_twist(self.latest_msg))
+            
+
+    def joy_to_twist(self, joy_msg):
+        
+        twist = Twist()
+        twist.linear.x = joy_msg.axes[1] * self.max_linear_speed
+        twist.linear.y = - joy_msg.axes[0] * self.max_linear_speed
+        twist.angular.z = joy_msg.axes[2] * self.max_angular_speed
+
+        return twist
+
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    holo_teleop_joy = HoloTeleopJoy()
+
+    rclpy.spin(holo_teleop_joy)
+
+    holo_teleop_joy.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ dev_tools/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'dev_tools'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='arusso',
+    maintainer_email='andrerusso02@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'holo_teleop_joy = dev_tools.holo_teleop_joy:main',
+        ],
+    },
+)
