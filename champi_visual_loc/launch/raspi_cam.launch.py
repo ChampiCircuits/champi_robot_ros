b397a7411f64dcb5c896e50c4be5617f6132626a
andrerusso02 <andrerusso02@gmail.com>
2024-02-09 18:32:38 +0100

    champi_visual_loc add launch file for robotik camera and calibration

---
+++ champi_visual_loc/launch/raspi_cam.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+import os
+
+def generate_launch_description():
+
+    calib_file = 'file://' + os.path.join(get_package_share_directory('champi_visual_loc'), 'calibration', 'raspi_cam_robotik.yaml')
+
+    print(calib_file)
+
+    return LaunchDescription([
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            name='v4l2_camera_node',
+            parameters=[
+                {"image_size": [640, 480]},
+                {"time_per_frame": [1, 30]},
+                {"camera_info_url": calib_file}
+        ]
+        )
+    ])
