1b88c4b554e16763bef4ac8b1c30cac122b7da0e
André Russo <andrerusso02@gmail.com>
2024-02-14 16:16:53 +0100

    dev(champi_libraries): create pkg + cmakelists for can_lib and test node

---
+++ champi_libraries/src/test_can_node.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "champi_can/msgs_can.pb.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+
+    cout << "Hello, World!" << endl;
+
+    return 0;
+}
