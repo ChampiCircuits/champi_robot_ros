2c39a05b349d73487fb75cfbd72864e4891bce13
EtienneClr-Projects <etienne.clairis@etik.com>
2024-05-02 19:23:36 +0200

    changes

--- champi_bringup/launch/bringup.launch.py
+++ champi_bringup/launch/bringup.launch.py
@@ -29,7 +29,7 @@ def generate_launch_description():
 
     # Get the URDF file TODO faire ça dans un launch file dans champi_description plutôt
     urdf_file_path = os.path.join(get_package_share_directory('champi_description'), 'urdf', 'champi.urdf')
-    urdf_content = open(urdf_file_path).read()    
+    urdf_content = open(urdf_file_path).read()
 
     description_broadcaster = Node(
             package='robot_state_publisher',
