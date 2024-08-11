fdf9fa461514d6ee23c947d09e1bfd4d9d589447
EtienneClr-Projects <etienne.clairis@etik.com>
2024-08-11 23:22:09 +0200

    page: usages of ram and cpu

--- champi_web_ui/scripts/modularization/pages/usages_page.py
+++ champi_web_ui/scripts/modularization/pages/usages_page.py
@@ -1,5 +1,4 @@
 import theme
-from message import message
 
 from nicegui import ui
 import psutil
@@ -11,27 +10,28 @@ from datetime import datetime
 
 def create() -> None:
     @ui.page('/usages')
-    def page_a():
+    def page_usages():
         with theme.frame('Usages'):
-            message('Usages')
+            with ui.element("div").style("width: 90%"):
+                ui.label('Usages').classes('text-h4 text-grey-8')
 
-            # limited to 60s
-            line_plot = ui.line_plot(n=2, limit=60, figsize=(10, 5), update_every=1) \
-                .with_legend(['cpu', 'ram'], loc='upper center', ncol=2)
+                # limited to 60s
+                line_plot = ui.line_plot(n=2, limit=60, figsize=(10, 5), update_every=1) \
+                    .with_legend(['cpu', 'ram'], loc='upper center', ncol=2)
 
-            cpu = []
-            ram = []
+                cpu = []
+                ram = []
 
-            def update_line_plot() -> None:
-                cpu_percent = psutil.cpu_percent()
-                ram_percent = psutil.virtual_memory().percent
-                cpu.append(cpu_percent)
-                ram.append(ram_percent)
-                now = datetime.now()
-                now = datetime.now().second
-                line_plot.push([now], [[cpu_percent], [ram_percent]])
+                def update_line_plot() -> None:
+                    cpu_percent = psutil.cpu_percent()
+                    ram_percent = psutil.virtual_memory().percent
+                    cpu.append(cpu_percent)
+                    ram.append(ram_percent)
+                    now = datetime.now()
+                    now = datetime.now().second
+                    line_plot.push([now], [[cpu_percent], [ram_percent]])
 
-            ui.timer(1.0, update_line_plot)
+                ui.timer(1.0, update_line_plot)
 
 #################################################
 #################### UTILS ######################
