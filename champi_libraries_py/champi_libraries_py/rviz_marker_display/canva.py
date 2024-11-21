eb7fde966b2c055dc2cf0dded04c96912c0a418f
andre <andrerusso02@gmail.com>
2024-11-21 18:51:32 +0100

    better version (inheritance from Marker)

--- champi_libraries_py/champi_libraries_py/rviz_marker_display/canva.py
+++ champi_libraries_py/champi_libraries_py/rviz_marker_display/canva.py
@@ -3,114 +3,239 @@ from rclpy.node import Node
 
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import Point, Pose
 
 from math import cos, sin
 import enum
 
+import random
+
+import champi_libraries_py.rviz_marker_display.colors as colors
+
+from icecream import ic
 
 THIN = 0.01
 MEDIUM = 0.02
 THICK = 0.05
 
 
-class Item:
+
+
+class MarkerWrapper(Marker):
     
     def __init__(self):
-        self.points_ros = []
-        self.type = None
-        self.color = None # TODO
-        self.scale = None # TODO
+        super().__init__()
 
+    
+    def __set_random_color(self):
+        self.color.r = random.random()
+        self.color.g = random.random()
+        self.color.b = random.random()
+        self.color.a = 1.
+    
+    def set_color(self, color: tuple):
+        if color is None:
+            self.__set_random_color()
+        else :
+            self.color.r = color[0] / 255.
+            self.color.g = color[1] / 255.
+            self.color.b = color[2] / 255.
+            self.color.a = 1.
+    
 
-    def set_points(self, points: list):
+    def __set_points_from_tuples_2d(self, points: list):
         for p in points:
             point_ros = Point()
             point_ros.x = float(p[0])
             point_ros.y = float(p[1])
-            if len(p) == 2:
-                point_ros.z = 0.
-            else:
-                point_ros.z = p[2]
-
-            self.points_ros.append(point_ros)
+            point_ros.z = 0.
+            self.points.append(point_ros)
     
 
-    def get_points(self):
-        return self.points_ros
+    def __set_points_from_tuples_3d(self, points: list):
+        for p in points:
+            point_ros = Point()
+            point_ros.x = float(p[0])
+            point_ros.y = float(p[1])
+            point_ros.z = float(p[2])
+            self.points.append(point_ros)
+
+    def set_points(self, points: list):
+        """accepts a 2D tuples, 3D tuples or list of ROS Points"""        
+
+        if len(points) == 0:
+            return
+        
+        if isinstance(points[0], Point):
+            self.points = points
+
+        elif isinstance(points[0], Pose):
+            for pose in points:
+                self.points.append(pose.position)
+
+        elif len(points[0]) == 2:
+            self.__set_points_from_tuples_2d(points)
+
+        elif len(points[0]) == 3:
+            self.__set_points_from_tuples_3d(points)
+        else:
+            raise ValueError("Invalid points")
+    
+
+    def set_pose(self, pose):
+
+        if isinstance(pose, Pose):
+            self.pose = pose
+        elif isinstance(pose, Point):
+            self.__set_pose_from_point(pose)
+        elif len(pose) == 2:
+            self.__set_pose_from_tuple_2d(pose)
+        elif len(pose) == 3:
+            self.__set_pose_from_tuple_3d(pose)
+        else:
+            raise ValueError("Invalid pose")
 
 
+    def __set_pose_from_tuple_3d(self, pose):
+        self.pose.position.x = float(pose[0])
+        self.pose.position.y = float(pose[1])
+        self.pose.position.z = float(pose[2])
+        self.pose.orientation.w = 1.
+    
+    def __set_pose_from_tuple_2d(self, pose):
+        self.pose.position.x = float(pose[0])
+        self.pose.position.y = float(pose[1])
+        self.pose.position.z = 0.
+        self.pose.orientation.w = 1.
+    
+    
+    def __set_pose_from_point(self, point):
+        self.pose.position = point
+        self.pose.orientation.w = 1.
 
 
-class Line(Item):
+    def get_base_marker(self):
+        """Necessary, because the publish method checks that argument is strictly a Marker
 
-    def __init__(self, start: tuple, end: tuple, line_width=MEDIUM):
+        Returns:
+            _type_: _description_
+        """
+        marker = Marker() # TODO use decorator instead ?
+        # Manually copy all attributes from the current object to the new one
+        for field in Marker.get_fields_and_field_types().keys():
+            setattr(marker, field, getattr(self, field))
+        return marker
+        
+
+    
+
+
+class Line(MarkerWrapper):
+    
+    def __init__(self, start, end, line_width=MEDIUM, color = None):
         super().__init__()
-        self.set_points([start, end])
         self.type = Marker.LINE_LIST
-        self.scale = [line_width, 0., 0.]
+        self.scale.x = float(line_width)
+        self.scale.y = 0.
+        self.scale.z = 0.
+        self.set_points([start, end])
+        self.set_color(color)
 
 
-class Polyline(Item):
 
-    def __init__(self, points: list, line_width=MEDIUM):
+class Polyline(MarkerWrapper):
+    
+    def __init__(self, points, line_width=MEDIUM, color=None):
         super().__init__()
-        self.set_points(points)
         self.type = Marker.LINE_STRIP
-        self.scale = [line_width, 0., 0.]
+        self.scale.x = line_width
+        self.scale.y = 0.
+        self.scale.z = 0.
+        self.set_points(points)
+        self.set_color(color)
 
 
-class Cube(Item):
+class Cube(MarkerWrapper):
+        
+    def __init__(self, pose, size, color=None):
+        super().__init__()
+        self.type = Marker.CUBE
+        self.scale.x = float(size[0])
+        self.scale.y = float(size[1])
+        self.scale.z = float(size[2])
+        self.set_pose(pose)
+        self.set_color(color)
 
-    def __init__(self, center: tuple, size: tuple[float, float, float], line_width=MEDIUM):
-        super().__init()
+
+class Sphere(MarkerWrapper):
+            
+    def __init__(self, pose, radius, color=None):
+        super().__init__()
+        self.type = Marker.SPHERE
+        self.scale.x = float(radius)
+        self.scale.y = float(radius)
+        self.scale.z = float(radius)
+        self.set_pose(pose)
+        self.set_color(color)
+
+
+# class Points(MarkerWrapper):
+        
+#     def __init__(self, poses, size=MEDIUM, color=None):
+#         super().__init__()
+#         self.type = Marker.POINTS
+#         self.scale.x = size
+#         self.scale.y = size
+#         self.scale.z = size
+#         self.set_points(poses)
+#         self.set_color(color)
+
+
+class Poses(MarkerWrapper):
+
+    class Types(enum.Enum):
+        POINTS = 1
+        CUBES = 2
+        SPHERES = 3
+        ARROWS = 4
+        FRAMES = 5
+    
+    class Sizes(enum.Enum):
+        SMALL = 0.1
+        MEDIUM = 0.2
+        LARGE = 0.3
+    
+    def __init__(self, poses, size=Sizes.LARGE, type=Types.POINTS, color=None):
+        super().__init__()
+        self.scale.x = size
+        self.scale.y = size
+        self.scale.z = size
+        if type == Poses.Types.POINTS:
+            self.type = Marker.POINTS
+        elif type == Poses.Types.CUBES:
+            self.type = Marker.CUBE_LIST
+        elif type == Poses.Types.SPHERES:
+            self.type = Marker.SPHERE_LIST
+        elif type == Poses.Types.ARROWS:
+            self.type = Marker.ARROW
+        elif type == Poses.Types.FRAMES:
+            pass #TODO
+        else:
+            raise ValueError("Invalid type")
+
+
+        self.set_points(poses)
+        self.set_color(color)
         
 
 
-# class Point(Item):
-    
-#         def __init__(self, point: tuple, scale: float = 0.1):
-#             super().__init__()
-#             self.set_points([point])
-#             self.type = Marker.POINTS
-#             self.scale = [scale, 0., 0.]
-
-
-# class Points(Item):
-        
-#         def __init__(self, points: list, scale: float = 0.1):
-#             super().__init__()
-#             self.set_points(points)
-#             self.type = Marker.POINTS
-#             self.scale = [scale, 0., 0.]
-
-
-class Rectangle(Polyline):
-    
-    def __init__(self, center, width, height, angle=0, line_width=MEDIUM):
-        
-        # Compute the 4 corners of the rectangle
-        x = center[0]
-        y = center[1]
-        w = width
-        h = height
-        p1 = [cos(angle) * w / 2 - sin(angle) * h / 2 + x, sin(angle) * w / 2 + cos(angle) * h / 2 + y]
-        p2 = [-cos(angle) * w / 2 - sin(angle) * h / 2 + x, -sin(angle) * w / 2 + cos(angle) * h / 2 + y]
-        p3 = [-cos(angle) * w / 2 + sin(angle) * h / 2 + x, -sin(angle) * w / 2 - cos(angle) * h / 2 + y]
-        p4 = [cos(angle) * w / 2 + sin(angle) * h / 2 + x, sin(angle) * w / 2 - cos(angle) * h / 2 + y]
-
-        # Fill Item properties
-        super().__init__([p1, p2, p3, p4, p1], width)
-
-    
-# TODO layers
 
 class Canva:
 
     def __init__(self, node, enable: bool):
         self.node = node
         self.enable = enable
-        self.items = []
+        self.markers = []
 
         self.cnt = 0
 
@@ -119,40 +244,24 @@ class Canva:
 
     def draw(self):
         
-        marker_array = MarkerArray()
-
-        if len(self.items) == 0:
+        if len(self.markers) == 0:
             return
-
-        for item in self.items:
-            marker_array.markers.append(self.__item_to_marker(item))
+        
+        marker_array = MarkerArray()
+        for marker in self.markers:
+            marker.header.frame_id = 'map'
+            marker.header.stamp = self.node.get_clock().now().to_msg()
+            marker.action = Marker.ADD
+            marker.id = self.cnt
+            marker_array.markers.append(marker.get_base_marker())
+            self.cnt += 1
         
         self.pub_marker.publish(marker_array)
 
-
     def clear(self):
-        self.items = []
-
-
-    def add(self, item: Item):
-        self.items.append(item)
+        self.markers = []
         self.cnt = 0
 
-    
-    def __item_to_marker(self, item: Item):
-        marker = Marker()
-        marker.header.frame_id = 'map'
-        marker.header.stamp = self.node.get_clock().now().to_msg()
-        marker.id = self.cnt
-        marker.type = item.type
-        marker.action = Marker.ADD
-        marker.points = item.get_points()
-        marker.color.a = 1.0
-        marker.color.r = 1.0
-        marker.scale.x = item.scale[0]
-        marker.scale.y = item.scale[1]
-        marker.scale.z = item.scale[2]
 
-        self.cnt += 1
-
-        return marker
+    def add(self, marker: Marker):
+        self.markers.append(marker)
--- dev_tools/scripts/test_markers.py
+++ dev_tools/scripts/test_markers.py
@@ -1,10 +1,9 @@
 import rclpy
 from rclpy.node import Node
 
-from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Point, Pose
 
 from champi_libraries_py.rviz_marker_display.canva import *
-import champi_libraries_py.rviz_marker_display.line_properties as line_properties
 
 
 
@@ -17,7 +16,35 @@ class TestNode(Node):
 
         self.canva = Canva(self, enable=True)
 
-        
+
+        pose1 = Pose()
+        pose1.position.x = 0.5
+        pose1.position.y = 0.5
+        pose1.position.z = 0.5
+        pose1.orientation.x = 0.1
+        pose1.orientation.y = 0.5
+        pose1.orientation.z = 0.5
+        pose1.orientation.w = 0.5
+
+        pose2 = Pose()
+        pose2.position.x = -0.5
+        pose2.position.y = -0.5
+        pose2.position.z = -0.5
+        pose2.orientation.x = -0.5
+        pose2.orientation.y = -0.5
+        pose2.orientation.z = -0.5
+        pose2.orientation.w = -0.5
+
+        pose3 = Pose()
+        pose3.position.x = 0.5
+        pose3.position.y = -0.5
+        pose3.position.z = 0.5
+        pose3.orientation.x = 0.5
+        pose3.orientation.y = -0.5
+        pose3.orientation.z = 0.5
+        pose3.orientation.w = 0.5
+
+        self.some_poses = [pose1, pose2, pose3]
 
         self.angle=0.
 
@@ -25,8 +52,12 @@ class TestNode(Node):
     def timer_callback(self):
 
         self.canva.clear()
-        self.canva.add(Rectangle((0, 0), 1, 1, self.angle))
-        self.canva.add(Line((0, 0), (1, 1), line_properties.line_thin))
+        self.canva.add(Line((-1, 0), (1, 0), line_width=MEDIUM, color=colors.RED))
+        self.canva.add(Polyline(self.some_poses, line_width=MEDIUM, color=colors.GREEN))
+        self.canva.add(Poses(self.some_poses, size= 0.2, color=colors.GREEN, type=Poses.Types.CUBES))
+
+        self.canva.add(Cube(self.some_poses[0], size=(0.1, 0.2, 0.3)))
+        self.canva.add(Sphere((0.5, 1), radius=0.4, color=colors.BLUE))
         self.canva.draw()
 
         self.angle += 0.1
