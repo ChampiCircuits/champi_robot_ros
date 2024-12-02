5ad5256ab87d270618d185c2f2dcef69d4418b4f
andre <andrerusso02@gmail.com>
2024-12-02 15:06:11 +0100

    modbus read and write work

--- champi_controllers/src/hardware_interface_node.cpp
+++ champi_controllers/src/hardware_interface_node.cpp
@@ -5,6 +5,26 @@
 
 using namespace std;
 
+
+struct Vector3
+{
+    float x;
+    float y;
+    float z;
+};
+
+uint16_t registers[100];
+
+const int address_cmd_vel = 0;
+const int data_len_cmd_vel = sizeof(Vector3) / sizeof(uint16_t);
+Vector3* cmd_vel = reinterpret_cast<Vector3*>(registers);
+
+const int address_measured_vel = address_cmd_vel + sizeof(Vector3) / sizeof(uint16_t);
+const int data_len_measured_vel = sizeof(Vector3) / sizeof(uint16_t);
+Vector3* measured_vel = reinterpret_cast<Vector3*>(registers + address_measured_vel);
+
+
+
 class ModbusSenderNode : public rclcpp::Node
 {
 public:
@@ -21,8 +41,8 @@ public:
         setup_modbus();
 
         timer_ = this->create_wall_timer(
-            std::chrono::seconds(1),
-            std::bind(&ModbusSenderNode::send_data, this));
+            std::chrono::milliseconds(20),
+            std::bind(&ModbusSenderNode::loop, this));
     }
 
     ~ModbusSenderNode()
@@ -60,8 +80,8 @@ private:
 
         RCLCPP_INFO(this->get_logger(), "Modbus RTU connection established with device: %s", device_.c_str());
 
-        uint32_t old_response_to_sec;
-        uint32_t old_response_to_usec;
+        // uint32_t old_response_to_sec;
+        // uint32_t old_response_to_usec;
 
         /* Save original timeout */
         // modbus_get_response_timeout(mb_, &old_response_to_sec, &old_response_to_usec);
@@ -71,14 +91,27 @@ private:
 
     }
 
-    void send_data()
+    void loop()
     {
-        const uint16_t data[3] = {1, 2, 3};
-        int rc = modbus_write_registers(mb_, 0, 3, data); // Writing to 3 consecutive registers starting at address 0
+
+        cmd_vel->x = 1.1;
+        cmd_vel->y = 2.2;
+        cmd_vel->z = 3.3;
+
+
+        int rc = modbus_write_registers(mb_, address_cmd_vel, data_len_cmd_vel, registers+address_cmd_vel);
         if (rc == -1) {
             RCLCPP_ERROR(this->get_logger(), "Failed to write data: %s", modbus_strerror(errno));
         } else {
-            RCLCPP_INFO(this->get_logger(), "Data sent successfully: [%d, %d, %d]", data[0], data[1], data[2]);
+            RCLCPP_INFO(this->get_logger(), "Data sent successfully");
+        }
+
+        rc = modbus_read_registers(mb_, address_measured_vel, data_len_measured_vel, registers+address_measured_vel);
+        if (rc == -1) {
+            RCLCPP_ERROR(this->get_logger(), "Failed to read data: %s", modbus_strerror(errno));
+        } else {
+            RCLCPP_INFO(this->get_logger(), "Data read successfully");
+            RCLCPP_INFO(this->get_logger(), "Measured velocity: x=%f, y=%f, z=%f", measured_vel->x, measured_vel->y, measured_vel->z);
         }
     }
 
