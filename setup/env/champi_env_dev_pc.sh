8de0693be02f0d6af657487d79d8bcbd6446eb31
andre <andrerusso02@gmail.com>
2025-04-18 13:18:33 +0200

    add rsync script

--- setup/env/champi_env_dev_pc.sh
+++ setup/env/champi_env_dev_pc.sh
@@ -5,3 +5,4 @@
 source ~/champi_ws/src/champi_robot_ros/setup/env/env_common.sh
 
 alias share_internet='~/champi_ws/src/champi_robot_ros/scripts/cmds/share_internet.sh'
+alias rsync_update='~/champi_ws/src/champi_robot_ros/scripts/cmds/rsync_update.bash'
