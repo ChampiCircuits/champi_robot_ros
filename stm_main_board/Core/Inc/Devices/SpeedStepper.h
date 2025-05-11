076f5e4096a6c37d496b2538f519c412e548cdfb
EtienneClr-Projects <etienne.clairis@etik.com>
2025-05-11 15:09:38 +0200

    posestepper can finally be dir reversed

--- stm_main_board/Core/Inc/Devices/SpeedStepper.h
+++ stm_main_board/Core/Inc/Devices/SpeedStepper.h
@@ -9,7 +9,7 @@ public:
 
 	SpeedStepper(); // default constructor, do not use.
 
-	SpeedStepper(TIM_HandleTypeDef tim_handle_step, uint32_t tim_channel_step, GPIO_TypeDef *gpio_port_dir, uint16_t gpio_pin_dir);
+	SpeedStepper(TIM_HandleTypeDef tim_handle_step, uint32_t tim_channel_step, GPIO_TypeDef *gpio_port_dir, uint16_t gpio_pin_dir, int inverse_dir=1);
 
 	void set_speed_step_freq(int hz, int dir);
 
@@ -33,7 +33,7 @@ private:
 
 	bool stopped;
 	float current_speed_rps;
-	int current_dir;
+	int inverse_dir;
 
 	uint32_t timer_input_hz;
 };
