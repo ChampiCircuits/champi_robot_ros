a8b99b8a14f9aafc2adcf5aa6c077a92ead77281
andre <andrerusso02@gmail.com>
2025-05-11 18:00:43 +0200

    make aruco code work again

--- champi_vision/scripts/aruco_loc_node.py
+++ champi_vision/scripts/aruco_loc_node.py
@@ -41,15 +41,11 @@ class VisualLocalizationNode(Node):
     def __init__(self):
         super().__init__('visual_loc')
 
-        self.get_logger().info("Launching visual localization node")
-
         # Parameters
-        self.enable_viz = True
-        self.angle_offset = 0.
+        self.enable_cv2_viz = True
+        self.enable_viz = False
+        self.img_viz = None
 
-
-
-        # transformation matrix between base_link and camera using tf2
         self.tf_buffer = tf2_ros.Buffer()
         self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
@@ -57,23 +53,13 @@ class VisualLocalizationNode(Node):
         self.map1 = None
         self.map2 = None
 
-        self.img_viz = None
+        # Bird view / image
         self.cv_bridge = CvBridge()
         self.curent_image = None
-        self.time_last_image = time.time()
+        self.latest_img = None
+        self.time_last_image = time.time()  # for FPS
         self.pos_cam_in_bird_view_pxls = None
         self.bird_view = None
-
-        ref_img_path = get_package_share_directory('champi_vision') + '/ressources/images/ref_img.png'
-        self.ref_img = self.load_ref_image(ref_img_path)
-
-        self.pxl_to_m_ref = self.ref_img.shape[1]/3.0
-
-        self.borders_offsets = [150, 360]
-
-        self.ref_img = cv2.copyMakeBorder(self.ref_img, self.borders_offsets[1], self.borders_offsets[1],
-                                          self.borders_offsets[0], self.borders_offsets[0],
-                                          cv2.BORDER_CONSTANT, value=[0, 0, 0])
 
         self.aruco_dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
         self.aruco_parameters =  cv2.aruco.DetectorParameters()
@@ -82,7 +68,7 @@ class VisualLocalizationNode(Node):
 
         self.timer = self.create_timer(0.1, self.timer_callback)  # 5Hz
 
-
+        # Subscribe to /camera_info and /image_raw---------------------------------------------------
 
         # handle camera info
         self.camera_info = None
@@ -92,32 +78,28 @@ class VisualLocalizationNode(Node):
             self.callback_cam_info,
             10)
 
-
-        self.subscription = self.create_subscription(
+        # handle image
+        self.image_subscriber = self.create_subscription(
             Image,
             '/camera/camera/color/image_raw',
             self.image_callback,
             10)
-        self.subscription  # prevent unused variable warning
 
+        self.image_subscriber  # prevent unused variable warning
+
+        # Publisher for the visual localization and the image visualization
         self.pub_odom = self.create_publisher(PoseWithCovarianceStamped, '/pose/visual_loc', 10)
-
+        self.bird_view_RGB_publisher = self.create_publisher(Image, '/champi_bird_view_RGB', 10)
         self.pub_image = self.create_publisher(Image, '/image_viz', 10)
 
-        self.latest_img = None
-
-        self.get_logger().info("Visual localization node launched !")
 
     def timer_callback(self):
-
         if self.latest_img is None:
             return
-        
 
         # get camera info if not already
         if self.camera_info is None:
-            # self.get_logger().info("waiting for camera info...", once=True)
-            self.get_logger().warn("waiting for camera info...")
+            self.get_logger().info("waiting for camera info...", once=True)
             return
         self.get_logger().info("camera info received", once=True)
 
@@ -129,9 +111,7 @@ class VisualLocalizationNode(Node):
         if self.map1 is None:
             return
 
-
         # ==================================== PREPROCESSING ====================================
-
         # convert to cv2
         cv_image = self.cv_bridge.imgmsg_to_cv2(self.latest_img, 'bgr8')
         cv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
@@ -146,105 +126,117 @@ class VisualLocalizationNode(Node):
         # get bird view
         bird_view_img = self.bird_view.project_img_to_bird(self.curent_image)
 
-        img_viz = bird_view_img.copy()
+        if self.enable_cv2_viz:
+            img_viz = bird_view_img.copy()
+            cv2.imshow('Bird view', bird_view_img)
+            cv2.waitKey(1)
 
+        
         # ================================== DETECTION =========================================
 
-        markerCorners, markerIds, rejectedCandidates = self.aruco_detector.detectMarkers(bird_view_img)
+        markerCorners, markerIds, _ = self.aruco_detector.detectMarkers(bird_view_img)
 
-
-        for i, corners in enumerate(markerCorners):
-            # self.get_logger().info(f"corners {corners}")
-            # self.get_logger().info(f"ids {markerIds[i]}")
+        #  if markers empty return
+        if markerIds is None:
+            self.get_logger().info("No marker detected", once=True)
+            return
         
-            if markerIds[i] >= 20 and markerIds[i]<=23:# arucos au sol
-                center_marker = np.mean(corners[0], axis=0)
-
-                # Angle du marqueur par rapport à l'axe horizontal (angle du premier côté)
-                dx = corners[0][1][0] - corners[0][0][0]
-                dy = corners[0][1][1] - corners[0][0][1]
-                angle_aruco_in_bv = np.arctan2(dy, dx)  # angle en radians
-
-                # self.get_logger().info(f"center_marker {center_marker}, angle {angle_rad*180/3.14}°")
-
-                pos_aruco_in_bv = np.array([center_marker[0], center_marker[1], 1])
-
-                pos_aruco_in_bv_m = np.linalg.inv(self.bird_view.M_workplane_real_to_img_) @ pos_aruco_in_bv
-                # ic(pos_aruco_in_bv_m)
-                # pos_m = pos_m[:2]
-                # ic(pos_m)
-
-                if markerIds[i]==20:
-                    x = 2.-0.45-0.12/2.
-                    y = 3.-0.7-0.12/2.
-                elif markerIds[i]==21:
-                    x = 2.-0.45-0.12/2.
-                    y = 0.7+0.12/2.
-                elif markerIds[i]==22:
-                    x = 0.5+0.12/2
-                    y = 3.-0.7-0.12/2.
-                elif markerIds[i]==23:
-                    x = 0.5+0.12/2
-                    y = 0.7+0.12/2.
-                
-                pos_aruco_in_world = np.array([x, y, 1])
-    
-                # Now we have the position of the aruco tag relative to the robot, and we know the position of the tag in the world.
-                # We can compute the position of the robot in the world frame. We do this computation using pos_aruco_in_bv_m, pos_aruco_in_world and angle_aruco_in_bv
-
-                # ic(pos_aruco_in_bv_m, pos_aruco_in_world, angle_aruco_in_bv)
-
-                # Compute the position of the robot in the world frame
-                pos_robot_in_world = pos_aruco_in_world - np.matmul(R.from_euler('z', angle_aruco_in_bv, degrees=False).as_matrix(), pos_aruco_in_bv_m)
-
-                # ic(pos_robot_in_world)
-
-                pose_robot = pos_robot_in_world
-
-
-                # ic(pose_robot, markerIds[i])
-
-                pose_msg = PoseWithCovarianceStamped()
-                pose_msg.header.stamp = self.get_clock().now().to_msg()
-                pose_msg.header.frame_id = 'odom'
-                pose_msg.pose.pose.position.x = pose_robot[0]
-                pose_msg.pose.pose.position.y = pose_robot[1]
-                pose_msg.pose.pose.position.z = 0.
-                pose_msg.pose.pose.orientation.x = 0.
-                pose_msg.pose.pose.orientation.y = 0.
-                pose_msg.pose.pose.orientation.z = np.sin(angle_aruco_in_bv/2)
-                pose_msg.pose.pose.orientation.w = np.cos(angle_aruco_in_bv/2)
-
-
-                # Very low covariance
-                cov = 0.02
-                for i in range(36):
-                    if i % 7 == 0:
-                        pose_msg.pose.covariance[i] = cov
-
-                self.pub_odom.publish(pose_msg)
-
-    
-                if self.enable_viz:
-
-                    # draw 2D axis
-                    img_viz = self.draw_2D_axis(img_viz, pos_aruco_in_bv[:2], angle_aruco_in_bv)
-                
+        marker_corner = None
+        marker_id = None
+        for i, markerId in enumerate(markerIds):
+            if markerId >= 20 and markerId <= 23:
+                marker_corner = markerCorners[i]
+                marker_id = markerId
                 break
+        if marker_corner is None:
+            self.get_logger().info("No marker detected", once=True)
+            return
+
+        # ========================= ARUCO -> ROBOT POSE ESTIMATION =====================================
+
+        # get the corners of the marker
+        corners = markerCorners[i]
+
+        # get the center of the marker
+        center_marker = np.mean(corners[0], axis=0)
+
+        # Marker angle relative to horizontal axis (first side angle)
+        dx = corners[0][1][0] - corners[0][0][0]
+        dy = corners[0][1][1] - corners[0][0][1]
+        angle_aruco_in_bv = np.arctan2(dy, dx)  # angle en radians
+
+        # self.get_logger().info(f"center_marker {center_marker}, angle {angle_rad*180/3.14}°")
+
+        pos_aruco_in_bv = np.array([center_marker[0], center_marker[1], 1])
+
+        pos_aruco_in_bv_m = np.linalg.inv(self.bird_view.M_workplane_real_to_img_) @ pos_aruco_in_bv
+
+        ic(pos_aruco_in_bv_m)
+
+        # ========================= ROBOT -> WORLD POSE ESTIMATION =====================================
+
+        if markerIds[i]==20:
+            x = 2.-0.45-0.12/2.
+            y = 3.-0.7-0.12/2.
+        elif markerIds[i]==21:
+            x = 2.-0.45-0.12/2.
+            y = 0.7+0.12/2.
+        elif markerIds[i]==22:
+            x = 0.5+0.12/2
+            y = 3.-0.7-0.12/2.
+        elif markerIds[i]==23:
+            x = 0.5+0.12/2
+            y = 0.7+0.12/2.
+
+        pos_aruco_in_world = np.array([x, y, 1])
+
+        # Now we have the position of the aruco tag relative to the robot, and we know the position of the tag in the world.
+        # We can compute the position of the robot in the world frame. We do this computation using pos_aruco_in_bv_m, pos_aruco_in_world and angle_aruco_in_bv
+
+        # ic(pos_aruco_in_bv_m, pos_aruco_in_world, angle_aruco_in_bv)
+
+        # Compute the position of the robot in the world frame
+        pos_robot_in_world = pos_aruco_in_world - np.matmul(R.from_euler('z', angle_aruco_in_bv, degrees=False).as_matrix(), pos_aruco_in_bv_m)
+
+        # ic(pos_robot_in_world)
+
+        pose_robot = pos_robot_in_world
+
+
+        # ic(pose_robot, markerIds[i])
+
+        pose_msg = PoseWithCovarianceStamped()
+        pose_msg.header.stamp = self.get_clock().now().to_msg()
+        pose_msg.header.frame_id = 'odom'
+        pose_msg.pose.pose.position.x = pose_robot[0]
+        pose_msg.pose.pose.position.y = pose_robot[1]
+        pose_msg.pose.pose.position.z = 0.
+        pose_msg.pose.pose.orientation.x = 0.
+        pose_msg.pose.pose.orientation.y = 0.
+        pose_msg.pose.pose.orientation.z = np.sin(angle_aruco_in_bv/2)
+        pose_msg.pose.pose.orientation.w = np.cos(angle_aruco_in_bv/2)
+
+
+        self.pub_odom.publish(pose_msg)
+
+        if self.enable_cv2_viz:
+            # draw 2D axis
+            img_viz = self.draw_2D_axis(img_viz, pos_aruco_in_bv[:2], angle_aruco_in_bv)
+            cv2.imshow('Detections', img_viz)
+            cv2.waitKey(1)
+
 
         if self.enable_viz:
 
             # publish image
-            self.publish_image(img_viz)
-
-
+            self.publish_imageMono(img_viz)
 
     def init_bird_view(self):
         transform = None
         try:
             # get transform, which is what we need to wait for
-            transform = self.tf_buffer.lookup_transform('base_link', 'camera',rclpy.time.Time().to_msg()) # todo use camera info
-
+            transform = self.tf_buffer.lookup_transform('base_link', 'camera_color_optical_frame',rclpy.time.Time().to_msg())
+            print('transform received')
             # # unsubscribe from camera info (can't do that in the callback otherwise the nodes crashes) TODO IT MAKES THE NODE CRASH
             # self.subscription_cam_info.destroy()
 
@@ -260,13 +252,13 @@ class VisualLocalizationNode(Node):
 
             # initialize bird view
             K = np.array(self.camera_info.k).reshape(3,3)
-            self.bird_view = bv.BirdView(K, transform_mtx, (0.27, -0.4), (0.82, 0.4), resolution=378)
+            self.bird_view = bv.BirdView(K, transform_mtx, (0., -0.4), (1., 0.4), resolution=378)
 
             self.pos_cam_in_bird_view_pxls = self.bird_view.get_work_plane_pt_in_bird_img(np.array([0, 0, 1]))
             ic(self.pos_cam_in_bird_view_pxls)
 
             # compute undistortion map
-            self.map1, self.map2 = cv2.initUndistortRectifyMap(K, np.array(self.camera_info.d), None, K, (640,480), cv2.CV_32FC1)
+            self.map1, self.map2 = cv2.initUndistortRectifyMap(K, np.array(self.camera_info.d), None, K, (self.camera_info.width,self.camera_info.height), cv2.CV_32FC1)
 
             self.get_logger().info("Node Initialized", once=True)
 
@@ -281,7 +273,11 @@ class VisualLocalizationNode(Node):
     def image_callback(self, msg):
         self.latest_img = msg
 
-    def publish_image(self, image):
+    def publish_imageRGB(self, image):
+        image_msg = self.cv_bridge.cv2_to_imgmsg(image, encoding='rgb8')
+        self.bird_view_RGB_publisher.publish(image_msg)
+
+    def publish_imageMono(self, image):
         image_msg = self.cv_bridge.cv2_to_imgmsg(image, encoding='mono8')
         self.pub_image.publish(image_msg)
 
@@ -300,8 +296,6 @@ class VisualLocalizationNode(Node):
 
         self.get_logger().info('Calling /set_pose...')
         self.future_set_pose_response = self.set_pose_client.call_async(request)
-
-
 
     def draw_2D_axis(self, image, pos_pxls, angle):
         img = image.copy()
