75fbd0c6a22ece41c206e71aa3f732a54041bcb8
EtienneClr-Projects <etienne.clairis@etik.com>
2025-05-11 17:53:10 +0200

    launch nav when CHAPI

--- setup/robot/tmux/champi_launch.bash
+++ setup/robot/tmux/champi_launch.bash
@@ -3,4 +3,4 @@
 source /opt/ros/jazzy/setup.bash
 source /home/champi/champi_ws/install/setup.bash
 
-ros2 launch champi_bringup bringup.launch.py sim:=false teleop:=true
+ros2 launch champi_bringup bringup.launch.py sim:=false nav:=True teleop:=true
