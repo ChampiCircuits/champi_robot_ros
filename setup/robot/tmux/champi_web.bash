b5638b5c71a8493cacba970299514a14c9355946
EtienneClr-Projects <etienne.clairis@etik.com>
2025-05-11 15:09:52 +0200

    deactivate web_ui auto launch for now

--- setup/robot/tmux/champi_web.bash
+++ setup/robot/tmux/champi_web.bash
@@ -4,14 +4,14 @@ source /opt/ros/jazzy/setup.bash
 source /home/champi/champi_ws/install/setup.bash
 sleep 20
 
-python3 ~/champi_ws/src/champi_robot_ros/champi_web_ui/scripts/modularization/main.py &
-
-sleep 2
-
-# Lancer Firefox dans la session graphique en mode kiosk
-XDG_RUNTIME_DIR=/run/user/$(id -u) systemd-run --user firefox --kiosk http://localhost:8080
-
-# Attendre que Firefox se ferme
-while pgrep firefox > /dev/null; do
-    sleep 1  # Vérifie toutes les secondes si Firefox est toujours en cours d'exécution
-done
+#python3 ~/champi_ws/src/champi_robot_ros/champi_web_ui/scripts/modularization/main.py &
+#
+#sleep 2
+#
+## Lancer Firefox dans la session graphique en mode kiosk
+#XDG_RUNTIME_DIR=/run/user/$(id -u) systemd-run --user firefox --kiosk http://localhost:8080
+#
+## Attendre que Firefox se ferme
+#while pgrep firefox > /dev/null; do
+#    sleep 1  # Vérifie toutes les secondes si Firefox est toujours en cours d'exécution
+#done
