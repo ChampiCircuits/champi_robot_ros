51b6c0ff9b22b56226ae4b450f6f34fe2e0415b5
andre <andrerusso02@gmail.com>
2025-05-13 19:55:32 +0200

    strat_displayer node !! and changes to web_ui

--- setup/robot/tmux/champi_web.bash
+++ setup/robot/tmux/champi_web.bash
@@ -2,13 +2,18 @@
 
 source /opt/ros/jazzy/setup.bash
 source /home/champi/champi_ws/install/setup.bash
+
+echo "Attente avant le démarrage complet du pc..."
 sleep 20
 
+echo "Lancement du serveur web sur le port 8080 !"
 python3 ~/champi_ws/src/champi_robot_ros/champi_web_ui/scripts/modularization/main.py &
 
 sleep 2
 
 # Lancer Firefox dans la session graphique en mode kiosk
+echo "Lancement de Firefox !"
+#XDG_RUNTIME_DIR=/run/user/$(id -u) systemd-run --user firefox --kiosk http://localhost:8080
 XDG_RUNTIME_DIR=/run/user/$(id -u) systemd-run --user firefox --kiosk http://localhost:8080
 
 # Attendre que Firefox se ferme
