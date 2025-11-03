f93eb543f22ba693869d956cce11fee1e9d97ff0
EtienneClr-Projects <etienne.clairis@etik.com>
2025-11-03 22:04:49 +0100

    [BUILD] Add mypy configuration and auto launch when building (at least for champi_brain)

--- setup/env/env_common.sh
+++ setup/env/env_common.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-
-alias build='~/champi_ws/src/champi_robot_ros/scripts/cmds/champi_build.sh'
-alias clean='rm -R ~/champi_ws/log ~/champi_ws/build ~/champi_ws/install'
+alias type-check='echo "🔍 Running type checks..." && python3 -m mypy champi_brain && echo "✅ Type checking complete!"' # only for packages ok, we'll expand later
+alias build='echo "📦 Sourcing environment..." && source ~/champi_ws/src/champi_robot_ros/setup/env/env_common.sh && echo "🔨 Building packages..." && ~/champi_ws/src/champi_robot_ros/scripts/cmds/champi_build.sh && type-check'
+alias clean='echo "🧹 Cleaning workspace..." && rm -R ~/champi_ws/log ~/champi_ws/build ~/champi_ws/install && echo "✅ Workspace cleaned!"'
 alias kill_nodes='~/champi_ws/src/champi_robot_ros/scripts/cmds/kill_nodes.bash'
 
 export RCUTILS_COLORIZED_OUTPUT=1
