40519806b5163163d9432d9c94147906301a91ee
andre <andrerusso02@gmail.com>
2025-11-22 12:40:34 +0100

    Not nice but wortking camera

--- champi_webots/launch/webots.launch.py
+++ champi_webots/launch/webots.launch.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python
 
 import os
-from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from launch import LaunchDescription
-from launch_ros.actions import Node
-import launch
-from ament_index_python.packages import get_package_share_directory, get_packages_with_prefixes
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription
+from ament_index_python.packages import get_package_share_directory
 from webots_ros2_driver.webots_launcher import WebotsLauncher
 from webots_ros2_driver.webots_controller import WebotsController
 from webots_ros2_driver.wait_for_controller_connection import WaitForControllerConnection
+from webots_ros2_driver.urdf_spawner import URDFSpawner
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
     package_dir = get_package_share_directory('champi_webots')
-    mode = LaunchConfiguration('mode')
+    mode = "realtime"
 
     # TODO It would be cleaner to copy files in share/ ...
     world_path = os.path.expanduser("~/champi_ws/src/champi_robot_ros/champi_webots/worlds/champi_webots.wbt")
@@ -28,9 +23,9 @@ def generate_launch_description():
         ros2_supervisor=True
     )
 
-    robot_description_path = os.path.join(package_dir, 'urdf', 'table.urdf')
+    robot_description_path = os.path.join(package_dir, 'urdf', 'watchtower.urdf')
     controller = WebotsController(
-        robot_name='Table',
+        robot_name='watchtower_1',
         parameters=[
             {'robot_description': robot_description_path,
              'use_sim_time': False,
@@ -38,18 +33,19 @@ def generate_launch_description():
         ],
         respawn=True
     )
+
+
+    # spawn_URDF_robot = URDFSpawner(
+    #     name='Table',
+    #     urdf_path=robot_description_path,
+    #     translation='0 0 1',
+    #     rotation='0 0 1 -1.5708',
+    # )
+
+
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'world',
-            default_value='turtlebot3_burger_example.wbt',
-            description='Choose one of the world files from `/webots_ros2_turtlebot/world` directory'
-        ),
-        DeclareLaunchArgument(
-            'mode',
-            default_value='realtime',
-            description='Webots startup mode'
-        ),
-        webots,
+        # webots,
         webots._supervisor,
         controller,
+        # spawn_URDF_robot,
     ])
