56050d5a05f429154c455427934fa5902a1a222c
EtienneClr-Projects <etienne.clairis@etik.com>
2026-03-14 23:48:59 +0100

    [ACTUATORS] Added thermometer actions

--- stm_main_board/Core/Inc/Application/Actuators/ActuatorsTask.h
+++ stm_main_board/Core/Inc/Application/Actuators/ActuatorsTask.h
@@ -3,6 +3,16 @@
 
 extern bool stop_all_actuators_requested;
 
+// THERMOMETER SERVO
+static constexpr int THERMO_SERVO_ID = 1;                     // TODO
+static constexpr int THERMO_SERVO_OPEN   = 0;    // ° [0,270] // TODO
+static constexpr int THERMO_SERVO_CLOSED = 90;  // ° [0,270] // TODO
+
 void ActuatorsTaskStart();
+
+void raiseThermometerServo();
+void lowerThermometerServo();
+void initEveryThing();
+void handleManualRequests();
 
 #endif //ACTUATORSTASK_H
--- stm_main_board/Core/Src/Application/Actuators/ActuatorsTask.cpp
+++ stm_main_board/Core/Src/Application/Actuators/ActuatorsTask.cpp
@@ -5,7 +5,6 @@
 #include "Application/Modbus/DataStructures.h"
 #include "Application/Modbus/ModbusRegister.h"
 #include "Application/Actuators/LiftAndClamp.h"
-#include "Application/PosSteppersTask.h"
 #include "Application/SCServosApp.h"
 #include "Application/Modbus/ModbusTask.h"
 #include "Application/Modbus/hw_actuators.h"
@@ -24,234 +23,39 @@ const osThreadAttr_t actuatorsTask_attributes = {
 };
 
 bool stop_all_actuators_requested = false;
+LiftAndClamp liftAndClamp;
 
-#define MAX_SERVO_END_ANGLE 155
-// max is 270°
-#define SERVO_END_LEFT_OPEN_POSITION 0 // TODO regler
-#define SERVO_END_LEFT_CLOSE_POSITION MAX_SERVO_END_ANGLE+8
-#define SERVO_END_RIGHT_OPEN_POSITION 270
-#define SERVO_END_RIGHT_CLOSE_POSITION 270-MAX_SERVO_END_ANGLE
-
-#define SERVO_ARM_UP_POSITION 205
-#define SERVO_ARM_DOWN_POSITION 92
-
-#define STEPPER_LOWER_POSITION 0.0
-
-#define SERVO_Y_RIGHT_OUT_POS 270
-#define SERVO_Y_RIGHT_IN_POS 145
-#define SERVO_Y_RIGHT_TAKE_POS SERVO_Y_RIGHT_IN_POS //150
-#define SERVO_Y_LEFT_OUT_POS 255
-#define SERVO_Y_LEFT_IN_POS 130
-#define SERVO_Y_LEFT_TAKE_POS SERVO_Y_LEFT_IN_POS //140
-
-struct ServoY
+void lowerThermometerServo()
 {
-    uint8_t id;
-    float angle_out;
-    float angle_in;
-    float angle_take;
-};
-
-ServoY SERVO_Y_LEFT = {ID_SERVO_Y_LEFT, SERVO_Y_LEFT_OUT_POS, SERVO_Y_LEFT_IN_POS, SERVO_Y_LEFT_TAKE_POS};
-ServoY SERVO_Y_RIGHT = {ID_SERVO_Y_RIGHT, SERVO_Y_RIGHT_OUT_POS, SERVO_Y_RIGHT_IN_POS, SERVO_Y_RIGHT_TAKE_POS};
-
-#define LOWER_PLANK 0
-#define UPPER_PLANK 1
-
-
-void InitYServos()
-{
-    devices::scs_servos::set_angle_async(SERVO_Y_LEFT.id, SERVO_Y_LEFT.angle_out, 300);
-    devices::scs_servos::set_angle(SERVO_Y_RIGHT.id, SERVO_Y_RIGHT.angle_out, 300);
-    devices::scs_servos::set_angle_async(SERVO_Y_LEFT.id, SERVO_Y_LEFT.angle_in, 300);
-    devices::scs_servos::set_angle(SERVO_Y_RIGHT.id, SERVO_Y_RIGHT.angle_in, 300);
+    devices::scs_servos::set_angle(THERMO_SERVO_ID, THERMO_SERVO_CLOSED, 300);
 }
-void InitLift()
+
+void raiseThermometerServo()
 {
-    devices::stepper_opt0.set_max_speed(5.0); //10
-    devices::stepper_opt0.set_max_accel(5.0); //5
+    devices::scs_servos::set_angle(THERMO_SERVO_ID, THERMO_SERVO_OPEN, 300);
+}
 
-    devices::stepper_opt0.set_zero();
-    devices::stepper_opt0.set_goal_sync(0.3);
-    bool lift_end_switch_released = HAL_GPIO_ReadPin(D6_GPIO_Port, D6_Pin);
 
-    if (lift_end_switch_released)
+void initEveryThing()
+{
+    LOG_INFO("act", "Beginning actuators initializing...");
+    osDelay(3000);
+    SCServosApp_Init(); // Reminder: blocking until the servos are found
+    osDelay(1000);
+
+    liftAndClamp.initialization();
+    // TODO others
+
+    LOG_INFO("act", "Actuators have been initialized !");
+}
+
+void HandleRequest(ActuatorCommand cmd)
+{
+    switch (cmd)
     {
-        devices::stepper_opt0.set_goal_async(-5.0);
-        while (lift_end_switch_released)
-        {
-            osDelay(10);
-            lift_end_switch_released = HAL_GPIO_ReadPin(D6_GPIO_Port, D6_Pin);
-        }
-        devices::stepper_opt0.set_zero();
-        devices::stepper_opt0.set_goal_async(0.0);
-    }
-
-    devices::stepper_opt0.set_max_speed(15.0); //10
-    devices::stepper_opt0.set_max_accel(20.0); //5
-}
-
-void InitEverything()
-{
-    LOG_INFO("act", "Initializing actuators...");
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_LEFT, SERVO_END_LEFT_OPEN_POSITION, 300);
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_RIGHT, SERVO_END_RIGHT_OPEN_POSITION, 300);
-
-    devices::scs_servos::set_angle(ID_SERVO_ARM, (SERVO_ARM_UP_POSITION+SERVO_ARM_DOWN_POSITION)/2.0, 300);
-
-    InitLift();
-
-    osDelay(10);
-    devices::scs_servos::set_angle_async(ID_SERVO_BANNER, 90, 300);
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM, SERVO_ARM_UP_POSITION, 300);
-    InitYServos();
-    LOG_INFO("act", "Initializing actuators done !");
-}
-
-void TakePlank(int plank) { // !!! lift should be down
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_RIGHT, SERVO_END_RIGHT_OPEN_POSITION, 300); // init au cas ou
-    devices::scs_servos::set_angle(ID_SERVO_ARM_END_LEFT, SERVO_END_LEFT_OPEN_POSITION, 500); // init au cas ou
-
-    float plank_height;
-    if (plank == LOWER_PLANK) {
-        plank_height = STEPPER_LOWER_POSITION;
-    }
-    else if (plank == UPPER_PLANK) {
-        plank_height = STEPPER_LOWER_POSITION + 0.45;
-    }
-
-    // CLOSED TO TAKE PLANK
-    devices::scs_servos::set_angle(ID_SERVO_ARM, SERVO_ARM_DOWN_POSITION, 300);
-
-    devices::stepper_opt0.set_goal_sync(plank_height);
-
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_RIGHT, SERVO_END_RIGHT_CLOSE_POSITION, 300);
-    devices::scs_servos::set_angle(ID_SERVO_ARM_END_LEFT, SERVO_END_LEFT_CLOSE_POSITION, 300);
-    osDelay(400);
-
-    if (plank == LOWER_PLANK) { // dans ce cas, pas besoin de remonter haut vu qu'on va prendre les conserves
-        devices::stepper_opt0.set_goal_async(0.37+0.7); // async pour passer dans l'état move plus tôt
-        osDelay(300);
-        devices::scs_servos::set_angle_async(SERVO_Y_LEFT.id, SERVO_Y_LEFT.angle_out, 600); // pour préparer la suite
-    }
-    else {
-        devices::stepper_opt0.set_goal_sync(3.6);
-    }
-}
-
-void PutPlanks(int layer)
-{
-    float layer_height;
-    if (layer == 1)
-        layer_height = STEPPER_LOWER_POSITION + 0.45; // hauteur de la upper plank comme ca ca fait un mouv en moins
-    else if (layer == 2)
-        layer_height = STEPPER_LOWER_POSITION + 3.4;
-
-    // CLOSED TO PUT PLANK
-    devices::scs_servos::set_angle(ID_SERVO_ARM, SERVO_ARM_DOWN_POSITION, 300);
-    devices::stepper_opt0.set_goal_sync(layer_height);
-
-
-    if (layer == 2) {
-        devices::scs_servos::set_angle(ID_SERVO_ARM, SERVO_ARM_DOWN_POSITION+23, 300);  // in this case we open a bit the arm to not destroy the tower
-    }
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_RIGHT, SERVO_END_RIGHT_OPEN_POSITION, 300);
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_LEFT, SERVO_END_LEFT_OPEN_POSITION, 300);
-}
-
-void TakeCanLeft(struct ServoY servo)
-{
-    devices::stepper_opt0.set_goal_sync(0.37);
-    devices::scs_servos::set_angle(servo.id, servo.angle_take, 300);
-    osDelay(300);
-    devices::stepper_opt0.set_goal_async(1.0); // async pour partir en move plus tôt
-
-    devices::scs_servos::set_angle_async(SERVO_Y_RIGHT.id, servo.angle_out, 300); // pour préparer la suite
-}
-
-void TakeCanRight(struct ServoY servo)
-{
-    devices::stepper_opt0.set_goal_sync(0.37);
-    devices::scs_servos::set_angle(servo.id, servo.angle_take, 300);
-    osDelay(300);
-    devices::stepper_opt0.set_goal_async(0.37+0.7); // async pour partir en move plus tôt
-}
-
-void PutCan(int layer, struct ServoY servo)
-{
-    float layer_base_height;
-    if (layer==1)
-        layer_base_height = 0.4;
-    else if (layer==2)
-        layer_base_height = 3.6;
-
-    devices::stepper_opt0.set_goal_sync(layer_base_height);
-    devices::scs_servos::set_angle(servo.id, servo.angle_out, 300);
-    // little movement to get the Y inside
-    devices::stepper_opt0.set_goal_sync(layer_base_height + 0.6);
-    devices::scs_servos::set_angle(servo.id, servo.angle_in, 300);
-    osDelay(700);
-
-//    devices::stepper_opt0.set_goal_sync(layer_base_height);
-//    devices::scs_servos::set_angle(servo.id, servo.angle_take+70., 300); // PUSH
-
-    // little movement to get the Y inside
-    // les deux async pour partir à l'état suivant plus tôt
-    if (layer==2) {
-        // devices::stepper_opt0.set_goal_async(layer_base_height + 0.6);
-//        devices::scs_servos::set_angle_async(servo.id, servo.angle_in, 300);
-    }
-    else {
-//        devices::scs_servos::set_angle(servo.id, servo.angle_in, 300);
-        devices::stepper_opt0.set_goal_sync(STEPPER_LOWER_POSITION + 0.45); // pour préparer le put lower plank
-    }
-}
-
-void PutBanner()
-{
-    devices::scs_servos::set_angle(ID_SERVO_BANNER, 250, 1000);
-}
-
-void getReady()
-{
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM, SERVO_ARM_DOWN_POSITION+33, 300);
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_RIGHT, SERVO_END_RIGHT_OPEN_POSITION, 300); // init au cas ou
-    devices::scs_servos::set_angle_async(ID_SERVO_ARM_END_LEFT, SERVO_END_LEFT_OPEN_POSITION, 300); // init au cas ou
-//    InitLift();
-    devices::stepper_opt0.set_goal_async(STEPPER_LOWER_POSITION + 0.45);
-}
-
-void HandleRequest(ActuatorCommand cmd) {
-    switch (cmd) {
-    case ActuatorCommand::PUT_BANNER:
-        PutBanner();
-        break;
-    case ActuatorCommand::TAKE_LOWER_PLANK:
-        TakePlank(LOWER_PLANK);
-        break;
-    case ActuatorCommand::TAKE_UPPER_PLANK:
-        TakePlank(UPPER_PLANK);
-        break;
-    case ActuatorCommand::PUT_LOWER_PLANK_LAYER_1:
-        PutPlanks(1);
-        break;
-    case ActuatorCommand::PUT_UPPER_PLANK_LAYER_2:
-        PutPlanks(2);
-        break;
-    case ActuatorCommand::TAKE_CANS_LEFT:
-        TakeCanLeft(SERVO_Y_LEFT);
-        break;
-    case ActuatorCommand::TAKE_CANS_RIGHT:
-        TakeCanRight(SERVO_Y_RIGHT);
-        break;
-    case ActuatorCommand::PUT_CANS_LEFT_LAYER_1:
-        PutCan(1, SERVO_Y_LEFT);
-        break;
-    case ActuatorCommand::PUT_CANS_RIGHT_LAYER_2:
-        PutCan(2, SERVO_Y_RIGHT);
-        break;
+        // TODO
     case ActuatorCommand::RESET_ACTUATORS:
-        InitEverything();
+        initEveryThing();
         break;
     case ActuatorCommand::STOP_ALL_MOTORS:
         stop_all_actuators_requested = true;
@@ -259,46 +63,43 @@ void HandleRequest(ActuatorCommand cmd) {
     case ActuatorCommand::ENABLE_ALL_MOTORS:
         stop_all_actuators_requested = false;
         break;
-    case ActuatorCommand::GET_READY:
-        getReady();
-        break;
     }
 }
 
-void ActuatorsTask(void *argument) {
-    LOG_INFO("act", "Begining actuators init...");
-    osDelay(3000);
-    //SCServosApp_Init(); // Reminder: blocking until the servos are found
-    osDelay(1000);
-    //InitEverything();
+void handleManualRequests()
+{
+    for (int i=0; i < static_cast<size_t>(ActuatorCommand::ACTUATORS_COUNT); i++)
+    {
+        xSemaphoreTake((QueueHandle_t)ModbusH.ModBusSphrHandle, portMAX_DELAY);
+        ActuatorState actuator_request = static_cast<ActuatorState>(mod_reg::actuators->requests[i]);
+        xSemaphoreGive(ModbusH.ModBusSphrHandle);
+
+        if (actuator_request == ActuatorState::REQUESTED)
+        {
+            ActuatorCommand actuator = static_cast<ActuatorCommand>(i);
+            LOG_INFO("act", "[MANUAL] Requested actuator %s to state %s", to_string(actuator).c_str(), to_string(actuator_request).c_str());
+            HandleRequest(actuator);
+            xSemaphoreTake((QueueHandle_t)ModbusH.ModBusSphrHandle, portMAX_DELAY);
+            mod_reg::actuators->requests[i] = static_cast<uint8_t>(ActuatorState::DONE);
+            xSemaphoreGive(ModbusH.ModBusSphrHandle);
+            LOG_INFO("act", "[MANUAL] Requested actuator %s to state %s", to_string(actuator).c_str(), to_string(static_cast<ActuatorState>(mod_reg::actuators->requests[i])).c_str());
+        }
+    }
+}
+
+void ActuatorsTask(void *argument)
+{
+    initEveryThing();
 
     LOG_INFO("act", "Starting loop.");
-
     while (true)
     {
-        // for (int i=0; i < ACTUATORS_COUNT; i++)
-        // {
-        //     xSemaphoreTake((QueueHandle_t)ModbusH.ModBusSphrHandle, portMAX_DELAY);
-        //     ActuatorState actuator_request = static_cast<ActuatorState>(mod_reg::actuators->requests[i]);
-        //     xSemaphoreGive(ModbusH.ModBusSphrHandle);
-
-        //     if (actuator_request == ActuatorState::REQUESTED)
-        //     {
-        //         ActuatorCommand actuator = static_cast<ActuatorCommand>(i);
-        //         LOG_INFO("act", "Actuator requested is %s to state %s", to_string(actuator).c_str(), to_string(actuator_request).c_str());
-        //         HandleRequest(actuator);
-        //         xSemaphoreTake((QueueHandle_t)ModbusH.ModBusSphrHandle, portMAX_DELAY);
-        //         mod_reg::actuators->requests[i] = static_cast<uint8_t>(ActuatorState::DONE);
-        //         xSemaphoreGive(ModbusH.ModBusSphrHandle);
-        //         LOG_INFO("act", "Actuator requested is %s to state %s", to_string(actuator).c_str(), to_string(static_cast<ActuatorState>(mod_reg::actuators->requests[i])).c_str());
-        //     }
-        // }
-        // // LOG_INFO("act", "Actuator requested is %s to state %s", to_string(static_cast<ActuatorCommand>(2)).c_str(), to_string(mod_reg::requests->actuators_state[2]).c_str());
-
+        handleManualRequests();
         osDelay(100);
     }
 }
 
-void ActuatorsTaskStart() {
+void ActuatorsTaskStart()
+{
     ActuatorsTaskHandle = osThreadNew(ActuatorsTask, NULL, &actuatorsTask_attributes);
 }
