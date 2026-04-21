cb3c06e84126ac7abf74ce094bdb443a0c07795f
EtienneClr-Projects <etienne.clairis@etik.com>
2026-04-21 23:12:38 +0200

    [PAMI] Update compilation for conditional logging

--- esp32_PAMIs/firmware/include/logging.h
+++ esp32_PAMIs/firmware/include/logging.h
@@ -3,7 +3,11 @@
 
 #include <Arduino.h>
 
-#if ENABLE_LOG_DEBUG
+#ifndef FIRMWARE_ENABLE_LOGS
+#define FIRMWARE_ENABLE_LOGS 0
+#endif
+
+#if FIRMWARE_ENABLE_LOGS
 
 #define LOG_DEBUG(logger, fmt, ...) \
 {Serial.printf("[%s] [DEBUG] " fmt "\n", logger, ##__VA_ARGS__);}
@@ -12,15 +16,6 @@
 {static int n = 0; \
 if (n==0) {Serial.printf("[%s] [DEBUG] " fmt "\n", logger, ##__VA_ARGS__);} \
 n += 1; n = n%N;}
-
-#else
-
-#define LOG_DEBUG(logger, fmt, ...) {}
-
-#define LOG_DEBUG_THROTTLE(logger, fmt, ...) {}
-
-#endif // ENABLE_LOG_DEBUG
-
 
 #define LOG_INFO(logger, fmt, ...) \
 {Serial.printf("[%s] [INFO] " fmt "\n", logger, ##__VA_ARGS__);}
@@ -45,5 +40,25 @@ n += 1; n = n%N;}
 {static int n = 0; \
 if (n==0) {Serial.printf("[%s] [ERROR] " fmt "\n", logger, ##__VA_ARGS__);} \
 n += 1; n = n%N;}
+
+#else
+
+#define LOG_DEBUG(logger, fmt, ...) {}
+
+#define LOG_DEBUG_THROTTLE(logger, N, fmt, ...) {}
+
+#define LOG_INFO(logger, fmt, ...) {}
+
+#define LOG_WARN(logger, fmt, ...) {}
+
+#define LOG_ERROR(logger, fmt, ...) {}
+
+#define LOG_INFO_THROTTLE(logger, N, fmt, ...) {}
+
+#define LOG_WARN_THROTTLE(logger, N, fmt, ...) {}
+
+#define LOG_ERROR_THROTTLE(logger, N, fmt, ...) {}
+
+#endif // FIRMWARE_ENABLE_LOGS
 
 #endif //FIRMWARE_LOGGING_H
